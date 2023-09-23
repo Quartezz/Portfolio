23693fd3af84e6b37483e42f4f16a0c7508b4672
Quartezz <kamil.machel03@gmail.com>
2023-09-23 15:01:48 +0200

    npx webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import handleScroll from "./handleScroll"
 
 window.addEventListener("scroll", handleScroll);
+
+const scrollToTopButton = document.getElementById('back-to-top');
+
+    scrollToTopButton.addEventListener('click', () => {
+        // Scroll to the top of the page with a smooth animation
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    });
