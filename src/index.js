3494b4b5eb1f1562e0894cf7b9522ad0fc8abf36
Quartezz <kamil.machel03@gmail.com>
2023-09-23 16:24:02 +0200

    media change

--- src/index.js
+++ src/index.js
@@ -11,3 +11,30 @@ const scrollToTopButton = document.getElementById('back-to-top');
             behavior: 'smooth',
         });
     });
+
+    document.addEventListener('DOMContentLoaded', function () {
+        // Find the button element
+        const jumpButton = document.getElementById('header-link');
+
+        // Add a click event listener to the button
+        jumpButton.addEventListener('click', function (e) {
+            e.preventDefault(); // Prevent the default behavior of the anchor link
+
+            // Get the target section's ID from the button's href attribute
+            const targetId = this.getAttribute('href').substring(1);
+
+            // Find the target section
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                // Calculate the target position
+                const targetPosition = targetSection.offsetTop;
+
+                // Scroll to the target position smoothly
+                window.scrollTo({
+                    top: targetPosition,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
