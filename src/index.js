fea123988c27fc3515209e660b1e41da184e4d52
Quartezz <kamil.machel03@gmail.com>
2023-09-25 12:40:13 +0200

    new function for animating

--- src/index.js
+++ src/index.js
@@ -1,40 +1,14 @@
-import handleScroll from "./handleScroll"
+import handleIntersection from "./handleIntersection"
+const projectItems = document.querySelectorAll('.project');
+const observerOptions = {
+    root: null, // Use the viewport as the root
+    rootMargin: '0px', // No margin
+    threshold: 0.5, // Trigger when at least 50% of the element is in the viewport
+};
 
-window.addEventListener("scroll", handleScroll);
+const observer = new IntersectionObserver(handleIntersection, observerOptions);
 
-const scrollToTopButton = document.getElementById('back-to-top');
-
-    scrollToTopButton.addEventListener('click', () => {
-        // Scroll to the top of the page with a smooth animation
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth',
-        });
-    });
-
-    document.addEventListener('DOMContentLoaded', function () {
-        // Find the button element
-        const jumpButton = document.querySelector('#header-link');
-
-        // Add a click event listener to the button
-        jumpButton.addEventListener('click', function (e) {
-            e.preventDefault(); // Prevent the default behavior of the anchor link
-
-            // Get the target section's ID from the button's href attribute
-            const targetId = this.getAttribute('href').substring(1);
-
-            // Find the target section
-            const targetSection = document.getElementById(targetId);
-
-            if (targetSection) {
-                // Calculate the target position
-                const targetPosition = targetSection.offsetTop;
-
-                // Scroll to the target position smoothly
-                window.scrollTo({
-                    top: targetPosition,
-                    behavior: 'smooth'
-                });
-            }
-        });
-    });
+// Observe each project item
+projectItems.forEach((item) => {
+    observer.observe(item);
+});
