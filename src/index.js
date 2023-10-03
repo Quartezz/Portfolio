886f09aaff782c619d85706b322a3a8a04907ab3
Quartezz <kamil.machel03@gmail.com>
2023-10-03 14:00:46 +0200

    deleted unused functions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import handleIntersection from "./handleIntersection"
-const projectItems = document.querySelectorAll('.project');
+import handleIntersection from "./handleIntersection";
+const projectItems = document.querySelectorAll(".project");
 const observerOptions = {
-    root: null, // Use the viewport as the root
-    rootMargin: '0px', // No margin
-    threshold: 0.5, // Trigger when at least 50% of the element is in the viewport
+  root: null, // Use the viewport as the root
+  rootMargin: "0px", // No margin
+  threshold: 0.5, // Trigger when at least 50% of the element is in the viewport
 };
 
 const observer = new IntersectionObserver(handleIntersection, observerOptions);
 
 // Observe each project item
 projectItems.forEach((item) => {
-    observer.observe(item);
+  observer.observe(item);
 });
